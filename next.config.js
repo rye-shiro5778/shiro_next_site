4137a0f0dccd2c2b3aea4ac6600fb9e3d5034a59
rye-shiro5778 <qd30755778@outlook.jp>
2022-06-23 18:04:19 +0900

    nextの設定変更

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  webpack(config, options) {
+    config.resolve.alias["@"] = path.join(__dirname, "src");
+    return config;
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontSize: {
+      // 'xs': '.75rem',
+      // 'sm': '.875rem',
+      // 'base': '1rem',
+      // 'lg': '1.125rem',
+      // 'xl': '1.25rem',
+      // '2xl': '1.5rem',
+      // '3xl': '1.875rem',
+      // '4xl': '2.25rem',
+      // '5xl': '3rem',
+      // '6xl': '4rem',
+      xs: ".75rem",
+      sm: "1rem",
+      base: "1.25rem",
+      lg: "1.5rem",
+      xl: "1.7rem",
+      "2xl": "1.875rem",
+      "3xl": "2.25rem",
+      "4xl": "3rem",
+      "5xl": "4rem",
+    },
   },
   plugins: [],
 };
