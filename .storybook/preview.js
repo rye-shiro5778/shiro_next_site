af1327bbce8c9f62903efd1c1ceaf4082c4ee788
rye-shiro5778 <qd30755778@outlook.jp>
2022-06-24 11:35:17 +0900

    dev設定の変更

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
+import * as NextImage from "next/image";
 import "../src/styles/globals.scss";
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => (
+    <OriginalNextImage {...props} placeholder={undefined} unoptimized />
+  ),
+});
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,6 +18,7 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  layout: "centered",
   backgrounds: {
     default: "default",
     values: [
