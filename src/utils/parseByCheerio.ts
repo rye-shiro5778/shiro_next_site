f605451f9b577cb75a2fdd3ff6944fbea47276c7
rye-shiro5778 <qd30755778@outlook.jp>
2022-07-05 01:34:27 +0900

    記事デザインの修正

--- src/utils/parseByCheerio.ts
+++ src/utils/parseByCheerio.ts
@@ -3,10 +3,15 @@ import hljs from "highlight.js";
 
 export function parseByCheerio(content: string): string {
   const cheerio = load(content);
+  cheerio("code").each((_, elm) => {
+    cheerio(elm).addClass("code-oneline");
+  });
   cheerio("pre code").each((_, elm) => {
     const result = hljs.highlightAuto(cheerio(elm).text());
     cheerio(elm).html(result.value);
     cheerio(elm).addClass("hljs");
+    cheerio(elm).removeClass("code-oneline");
   });
+
   return cheerio.html();
 }
