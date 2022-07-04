227dd4373bf299aa7e000e8238ba6d267c054472
rye-shiro5778 <qd30755778@outlook.jp>
2022-07-04 16:25:09 +0900

    設定の変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,20 @@ module.exports = {
       sm: "1rem",
       base: "1.25rem",
       lg: "1.5rem",
-      xl: "1.7rem",
+      xl: "1.75rem",
       "2xl": "1.875rem",
       "3xl": "2.25rem",
       "4xl": "3rem",
       "5xl": "4rem",
       "6xl": "5rem",
+    },
+    letterSpacing: {
+      tighter: "-.05em",
+      tight: "-.025em",
+      normal: "0",
+      wide: ".025em",
+      wider: ".05em",
+      widest: ".1em",
     },
     keyframes: {
       bounce100: {
