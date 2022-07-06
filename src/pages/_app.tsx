51e3879dd411f95dfe1ef567e9ebe6e88b21a445
rye-shiro5778 <qd30755778@outlook.jp>
2022-07-06 14:56:40 +0900

    GA4設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,49 @@
 import type { AppPropsWithLayout } from "next/app";
+import { useRouter } from "next/router";
+import Script from "next/script";
+import { useEffect } from "react";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout || ((page) => page);
+  const router = useRouter();
+  const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID || "";
 
-  return getLayout(<Component {...pageProps} />);
+  useEffect(() => {
+    if (!GA_TRACKING_ID) return;
+    const handleRouteChange = (url: string) => {
+      window.gtag("config", GA_TRACKING_ID, {
+        page_path: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events, GA_TRACKING_ID]);
+
+  const getLayout = Component.getLayout || ((page) => page);
+  return getLayout(
+    <>
+      {GA_TRACKING_ID && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <Script id="ga" defer strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());    
+              gtag('config', '${GA_TRACKING_ID}');
+          `}
+          </Script>
+        </>
+      )}
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
