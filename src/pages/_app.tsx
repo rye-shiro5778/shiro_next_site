a7ec9e8beb20f2b98f91fb7f5c33f32a3c4a1625
rye-shiro5778 <qd30755778@outlook.jp>
2022-07-11 18:52:03 +0900

    不要削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,8 @@
 import { useGoogleAnalytics } from "@/hooks/useGoogleAnalytics";
 import type { AppPropsWithLayout } from "next/app";
-import { useRouter } from "next/router";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  const router = useRouter();
   const { GoogleAnalyticsHead } = useGoogleAnalytics();
 
   const getLayout = Component.getLayout || ((page) => page);
