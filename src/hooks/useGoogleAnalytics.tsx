1e3a6a627ffda95acdf2e87f6195f09bb141914b
rye-shiro5778 <qd30755778@outlook.jp>
2022-08-22 17:57:12 +0900

    余白整形

--- src/hooks/useGoogleAnalytics.tsx
+++ src/hooks/useGoogleAnalytics.tsx
@@ -30,11 +30,11 @@ export function useGoogleAnalytics() {
           />
           <Script id="ga" defer strategy="afterInteractive">
             {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());    
-          gtag('config', '${GA_TRACKING_ID}');
-      `}
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());    
+              gtag('config', '${GA_TRACKING_ID}');
+            `}
           </Script>
         </>
       )}
